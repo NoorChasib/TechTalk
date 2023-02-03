7f5623c65aba3c8650aac0c523df1a50a0a36ba7
Bilal Farah <bfarah@ualberta.ca>
2023-02-03 16:16:00 -0700

    update right menu bar

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -1,9 +1,27 @@
 import "./rightBar.scss"
 
-const RightBar = () =>{
-  return(
-    <div className="rightBar">RightBar</div>
-  )
-}
+export const RightBar = () => {
+  return (
+    <div className="rightBar"> 
+      <div className="container">
+        <div className="item"> 
+          <span>Suggested For You</span> 
+          <div className="user"> 
+            <div className="userInfo"> 
+              <img src="https://www.theladders.com/wp-content/uploads/jeff-bezos-ceo-profile-800x450.jpg" alt="" /> 
+              <span>Jeff Bezos</span>
+            </div>
+            <div className="buttons"> 
+              <button>Follow</button> 
+              <button>Ignore</button>
+              
+            </div>
 
-export default RightBar
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+  )
+} 
