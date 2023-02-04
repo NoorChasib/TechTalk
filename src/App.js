f4cb573a3ea6dadb1993fb9afd125883b292eeac
Bilal Farah <bfarah@ualberta.ca>
2023-02-03 18:09:39 -0700

    set right bar

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Home from "./pages/home/home";
 
 function App() {
 
-  const currentUser = false;
+  const currentUser = true;
 
   const Layout = () => {
     return (
--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -1,27 +1,151 @@
-import "./rightBar.scss"
+import './rightBar.scss';
 
-export const RightBar = () => {
+const RightBar = () => {
   return (
-    <div className="rightBar"> 
+    <div className="rightBar">
       <div className="container">
-        <div className="item"> 
-          <span>Suggested For You</span> 
-          <div className="user"> 
-            <div className="userInfo"> 
-              <img src="https://www.theladders.com/wp-content/uploads/jeff-bezos-ceo-profile-800x450.jpg" alt="" /> 
+        {/* <div className="item">
+          <img
+            src="https://www.theladders.com/wp-content/uploads/jeff-bezos-ceo-profile-800x450.jpg"
+            alt=""
+          />
+        </div> */}
+        <div className="item">
+          <span>Suggested For You</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://www.theladders.com/wp-content/uploads/jeff-bezos-ceo-profile-800x450.jpg"
+                alt=""
+              />
               <span>Jeff Bezos</span>
             </div>
-            <div className="buttons"> 
-              <button>Follow</button> 
+            <div className="buttons">
+              <button>Follow</button>
               <button>Ignore</button>
-              
             </div>
-
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://ichef.bbci.co.uk/news/976/cpsprodpb/B7F6/production/_128049074_muskgetty.png"
+                alt=""
+              />
+              <span>Elon Musk</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Ignore</button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://ichef.bbci.co.uk/news/976/cpsprodpb/B7F6/production/_128049074_muskgetty.png"
+                alt=""
+              />
+              <p>
+                <span>Bill Gates</span> liked a post
+              </p>
+            </div>
+            <span>1 min ago</span>
           </div>
 
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://ichef.bbci.co.uk/news/976/cpsprodpb/B7F6/production/_128049074_muskgetty.png"
+                alt=""
+              />
+              <p>
+                <span>John Doe</span> liked commented on a post
+              </p>
+            </div>
+            <span>4 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://ichef.bbci.co.uk/news/976/cpsprodpb/B7F6/production/_128049074_muskgetty.png"
+                alt=""
+              />
+              <p>
+                <span>Jack Dorsey</span> liked a post
+              </p>
+            </div>
+            <span>5 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://ichef.bbci.co.uk/news/976/cpsprodpb/B7F6/production/_128049074_muskgetty.png"
+                alt=""
+              />
+              <p>
+                <span>Larry Ellison</span> shared a post
+              </p>
+            </div>
+            <span>9 min ago</span>
+          </div>
+        </div>
+        <div className="item">
+          <span>Active friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://ichef.bbci.co.uk/news/976/cpsprodpb/B7F6/production/_128049074_muskgetty.png"
+                alt=""
+              />
+              <div className="online" />
+              <span>Bill Gates</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://ichef.bbci.co.uk/news/976/cpsprodpb/B7F6/production/_128049074_muskgetty.png"
+                alt=""
+              />
+              <div className="online" />
+              <span>Larry Ellison</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://ichef.bbci.co.uk/news/976/cpsprodpb/B7F6/production/_128049074_muskgetty.png"
+                alt=""
+              />
+              <div className="online" />
+              <span>Sergey Brin</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://ichef.bbci.co.uk/news/976/cpsprodpb/B7F6/production/_128049074_muskgetty.png"
+                alt=""
+              />
+              <div className="online" />
+              <span>Mark Zuckerberg</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://ichef.bbci.co.uk/news/976/cpsprodpb/B7F6/production/_128049074_muskgetty.png"
+                alt=""
+              />
+              <div className="online" />
+              <span>Bernard Arnault</span>
+            </div>
+          </div>
         </div>
       </div>
-
     </div>
-  )
-} 
+  );
+};
+export default RightBar;
