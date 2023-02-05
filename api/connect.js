a68457adbad22286a8462c78c8c64fcef5084ea4
Noor Chasib <noorchasib@gmail.com>
2023-02-05 14:05:10 -0800

    updates to env

--- api/connect.js
+++ api/connect.js
@@ -1,9 +1,12 @@
 import mysql from 'mysql';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../api/.env' });
 
 export const db = mysql.createConnection({
-  host: 'localhost',
-  port: '3306',
-  user: 'root',
-  password: '@mysql123',
-  database: 'social',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
