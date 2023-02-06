60e35786c570eb6f0238d57023c0189eb4c4d1f3
Awo <amoussab@ualberta.ca>
2023-02-06 12:18:49 -0700

    added new post feature

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,5 +1,6 @@
 import { db } from '../connect.js';
 import jwt from 'jsonwebtoken';
+import moment from 'moment';
 
 export const getPosts = (req, res) => {
   const token = req.cookies.accessToken;
@@ -14,6 +15,30 @@ export const getPosts = (req, res) => {
     db.query(q, [userInfo.id, userInfo.id], (err, data) => {
       if (err) return res.status(500).json(err);
       return res.status(200).json(data);
+    });
+  });
+};
+
+export const addPost = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json('Not logged in!');
+
+  jwt.verify(token, 'secretkey', (err, userInfo) => {
+    if (err) return res.status(403).json('Token is not valid!');
+
+    const q =
+      "INSERT INTO posts (`desc`, `img`, `createdAt`,`userId`) VALUES (?) ";
+
+    const values = [
+      req.body.desc,
+      req.body.img,
+      moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'),
+      userInfo.id,
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json('Post has been created');
     });
   });
 };
