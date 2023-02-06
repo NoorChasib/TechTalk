a2c416eb3028cb709ca96b1d05138675353a57a9
Noor Chasib <noorchasib@gmail.com>
2023-02-05 21:31:00 -0800

    seeds

---
+++ api/db/seeds/posts.sql
@@ -0,0 +1,12 @@
+INSERT INTO `posts` (`id`,`desc`,`userId`)
+VALUES
+  (1,"Nullam velit dui, semper et, lacinia vitae, sodales at,",10),
+  (2,"Aliquam erat volutpat. Nulla facilisis. Suspendisse commodo tincidunt nibh.",2),
+  (3,"vitae aliquam eros turpis non enim. Mauris quis",3),
+  (4,"Mauris quis turpis vitae purus gravida",2),
+  (5,"enim. Suspendisse aliquet, sem",6),
+  (6,"placerat velit. Quisque varius. Nam porttitor scelerisque neque.",6),
+  (7,"orci luctus et ultrices posuere cubilia Curae",9),
+  (8,"Phasellus in felis. Nulla",4),
+  (9,"Donec at arcu. Vestibulum ante ipsum primis in faucibus",2),
+  (10,"feugiat. Sed nec metus facilisis",2);
