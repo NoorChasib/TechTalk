b137464158b6976dae55e174ed67db78fb24d5e2
Awo <amoussab@ualberta.ca>
2023-02-07 02:18:57 -0700

    created delete post feature

--- client/src/components/post/post.jsx
+++ client/src/components/post/post.jsx
@@ -22,6 +22,7 @@ import { AuthContext } from '../../context/authContext';
 
 const Post = ({ post }) => {
   const [commentOpen, setCommentOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const { currentUser } = useContext(AuthContext);
 
@@ -45,8 +46,25 @@ const Post = ({ post }) => {
       },
     }
   );
+
+  const deleteMutation = useMutation(
+    (postId) => {
+      return makeRequest.delete('/posts/' + postId);
+    },
+    {
+      onSuccess: () => {
+        // Invalidate and refetch
+        queryClient.invalidateQueries(['posts']);
+      },
+    }
+  );
+
   const handleLike = () => {
     mutation.mutate(data.includes(currentUser.id));
+  };
+
+  const handleDelete = () => {
+    deleteMutation.mutate(post.id);
   };
 
   return (
@@ -66,11 +84,15 @@ const Post = ({ post }) => {
             </div>
           </div>
           <FontAwesomeIcon
+            onClick={() => setMenuOpen(!menuOpen)}
             icon={faEllipsis}
             className="faIcon"
             size="lg"
             fixedWidth
           />
+          {menuOpen && post.userId === currentUser.id && (
+            <button onClick={handleDelete}>delete?</button>
+          )}
         </div>
         <div className="content">
           <p>{post.desc}</p>
