ede2ee2bd58c18080bfa76abcf3ea29114239c7d
Noor Chasib <noorchasib@gmail.com>
2023-02-06 20:41:57 -0800

    implement new design language

--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -3,62 +3,64 @@ import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/authContext';
 import './login.scss';
 
-const Login = () => { 
-  
+const Login = () => {
   const [inputs, setInputs] = useState({
-    username: "",
-    password: "",
+    username: '',
+    password: '',
   });
   const [err, setErr] = useState(null);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
   const { login } = useContext(AuthContext);
 
-  const handleLogin = async (e) => {  
-    e.preventDefault()
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
     try {
-      await login(inputs) 
-      navigate("/")
-    } catch(err) {
-      setErr(err.response.data)
+      await login(inputs);
+      navigate('/');
+    } catch (err) {
+      setErr(err.response.data);
     }
   };
   return (
     <div className="login">
       <div className="card">
         <div className="left">
+          <div className="logo">
+            <img src={require('../..//assets/logo.png')} alt="TechTalk" />
+            {/* <h4>TechTalk</h4> */}
+          </div>
           <h1> Empowering Developers to Connect, Learn, and Grow</h1>
-          <p>
-            {/* Join forces with a group of individuals sharing a common interest in web development. Let's work together and strive for growth. */}
-          </p>
-          <span> Don't have an account?</span>
-          <Link to="/register">
-            <button> Register </button>
-          </Link>
+          <div className="account">
+            <span> Don't have an account?</span>
+            <Link to="/register">
+              <button> Register </button>
+            </Link>
+          </div>
         </div>
         <div className="right">
-          <h1> Login </h1>
+          <h1> Welcome Back </h1>
           <form>
-            <input 
-            type="text" 
-            placeholder="Username" 
-            name="username"
-            onChange={handleChange}
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
             />
-            <input 
-            type="password" 
-            placeholder="Password" 
-            name="password"
-            onChange={handleChange}
-            /> 
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+            />
             {err && err}
-            <button onClick={handleLogin}>Login</button>
           </form>
+          <button onClick={handleLogin}>Login</button>
         </div>
       </div>
     </div>
