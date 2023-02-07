cdba9b7b3eeba241193d6c3ea009643c4e1bef03
Awo <amoussab@ualberta.ca>
2023-02-07 01:50:54 -0700

    made changes to update feature

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -11,13 +11,15 @@ export const getPosts = (req, res) => {
   jwt.verify(token, 'secretkey', (err, userInfo) => {
     if (err) return res.status(403).json('Token is not valid!');
 
-    const q = userId
+    const q = 
+    userId !== "undefined"
       ? `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.createdAt DESC`
       : `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
   LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId= ? OR p.userId =?
   ORDER BY p.createdAt DESC`;
 
-    const values = userId ? [userId] : [userInfo.id, userInfo.id];
+    const values = 
+    userId !== "undefined" ? [userId] : [userInfo.id, userInfo.id];
 
     db.query(q, values, (err, data) => {
       if (err) return res.status(500).json(err);
