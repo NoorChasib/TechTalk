bb3f4c9f5acdebe46b8774a32c688baf62038c7b
Noor Chasib <noorchasib@gmail.com>
2023-02-09 18:14:32 -0800

    created first chat box

--- client/src/pages/chat/chat.jsx
+++ client/src/pages/chat/chat.jsx
@@ -5,12 +5,10 @@ const Chat = () => {
     <div className="chat">
       <div className="chatFront">
         <div className="chatContainer">
-          <div className='chatInnerContainer'>
-            <div className="chatBox">
+          <div className='chatBox'>
             <p>
             hi
             </p>
-            </div>
             </div>
         </div>
       </div>
