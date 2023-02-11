42ae3b6546aef9c91079604ae19a6709f398e806
Noor Chasib <noorchasib@gmail.com>
2023-02-11 01:00:54 -0800

    add chat width and height properties and overall look

--- client/src/components/message/message.jsx
+++ client/src/components/message/message.jsx
@@ -1,17 +1,17 @@
 import './message.scss';
 
-const Message = () => {
+const Message = ({own}) => {
   return (
-    <div className="message">
+    <div className={own ? "message own" : "message"}>
       <div className="messageTop">
         <img
+          className="messageImg"
           src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
           alt=""
-          className="messageImg"
         />
-        <p className="messageText">Hello World</p>
+        <p className="messageText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis ipsum similique excepturi sint quam molestias sit temporibus qui voluptas quod. Officia laboriosam doloremque ad facere, illum tempora aspernatur blanditiis rerum.</p>
       </div>
-      <div className="messageBottom">Hello World</div>
+      <div className="messageBottom">1 Hour ago</div>
     </div>
   );
 };
--- client/src/pages/messenger/messenger.jsx
+++ client/src/pages/messenger/messenger.jsx
@@ -1,16 +1,17 @@
 import './messenger.scss';
 import Conversation from '../../components/conversations/conversation';
+import Message from '../../components/message/message';
 
 const Messenger = () => {
   return (
-    <div className="messenger">
-      <div className="messengerFront">
-        <div className="messengerContainer">
-          <div className="messengerMenu">
-            <div className="messengerNoWrapper">
+    <div className="chat">
+      <div className="chatFront">
+        <div className="chatContainer">
+          <div className="chatMenu">
+            <div className="chatNoWrapper">
               <input
                 placeholder="search for friends"
-                className="messengerMenuInput"
+                className="chatMenuInput"
               />
               <Conversation />
               <Conversation />
@@ -18,13 +19,35 @@ const Messenger = () => {
               <Conversation />
             </div>
           </div>
-          <div className="messengerBox">
-            <div className="messengerBoxWrapper">
-              <div className="messengerBoxTop"></div>
+          <div className="chatBox">
+            <div className="chatBoxWrapper">
+              <div className="chatBoxTop">
+                <Message />
+                <Message own={true} />
+                <Message />
+                <Message />
+                <Message />
+                <Message />
+                <Message />
+                <Message />
+                <Message />
+                <Message />
+                <Message />
+                <Message />
+                <Message />
+                <Message />
+              </div>
+              <div className="chatBoxBottom">
+                <textarea
+                  className="chatMessageInput"
+                  placeholder="Write something..."
+                ></textarea>
+                <button className="chatSubmitButton">Send</button>
+              </div>
             </div>
           </div>
-          <div className="messengerOnline">
-            <div className="messengerNoWrapper">online</div>
+          <div className="chatOnline">
+            <div className="chatNoWrapper">online</div>
           </div>
         </div>
       </div>
