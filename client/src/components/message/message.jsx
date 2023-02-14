e5ab7a393586c40961ee090c8e030a0e58812ad0
Noor Chasib <noorchasib@gmail.com>
2023-02-14 00:58:33 -0800

    added ability to show each users profile picture

--- client/src/components/message/message.jsx
+++ client/src/components/message/message.jsx
@@ -1,16 +1,42 @@
 import './message.scss';
-import { memo } from 'react';
+import { memo, useContext, useEffect, useState } from 'react';
 import moment from 'moment';
+import { AuthContext } from '../../context/authContext';
+import { makeRequest } from '../../axios';
+
+const getOtherUserId = (message, currentUser) => {
+  if (message.sender !== currentUser.id) {
+    return message.sender;
+  }
+};
 
 const Message = ({ message, own }) => {
+  const { currentUser } = useContext(AuthContext);
+  const [user, setUser] = useState(null);
+  const otherUsersId = getOtherUserId(message, currentUser);
+
+  useEffect(() => {
+    if (otherUsersId) {
+      const getUser = async () => {
+        try {
+          const res = await makeRequest.get('/users/find/' + otherUsersId);
+          setUser(res.data);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      getUser();
+    }
+  }, [otherUsersId]);
+
+  const imgSrc = own
+    ? '/upload/' + currentUser.profilePic
+    : `/upload/${user?.profilePic}`;
+
   return (
     <div className={own ? 'message own' : 'message'}>
       <div className="messageTop">
-        <img
-          className="messageImg"
-          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-          alt=""
-        />
+        <img className="messageImg" src={imgSrc} alt="" />
         <p className="messageText">{message.text}</p>
       </div>
       <div className="messageBottom">{moment(message.createdAt).fromNow()}</div>
